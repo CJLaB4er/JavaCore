4b5a70bbd934e94543cea1a2be8da85cba14cb54
Бронислав Scomi <cjlab4er@gmail.com>
2023-10-27 21:56:06 +0500

    Выполнено первое задание семинара

---
+++ src/main/java/sem2/Main.java
@@ -0,0 +1,10 @@
+package sem2;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = task1.createArray(10);
+
+        task1.printResult(arr, task1.searchMinMax(arr));
+
+    }
+}
